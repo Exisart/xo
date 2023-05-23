62eea5e099bfd45089c2c9b06d409bdb47a5b08b
Exisart <greshnik423>
2023-05-23 16:54:16 +0300

    work all without logic

--- controller/functions.php
+++ controller/functions.php
@@ -8,6 +8,33 @@ function setX() {
   }
 }
 
+function setO() {
+  $i = 0;
+  foreach ($_SESSION['array'] as $value) {
+    if ($value == 'x') {
+      $i++;
+    }
+  }
+
+  if ($i === 1) { // 1
+
+  }
+
+  if ($i === 2) { // 2
+
+  }
+
+  if ($i === 3) { // 3
+
+  }
+
+  if ($i === 4) { // 4
+
+  }
+
+  //$_SESSION['array'][$key] = 'o';
+}
+
 function createArray() {
   $_SESSION['array'] = array(
     1 => '?',
@@ -26,8 +53,26 @@ function winner() {
   if (
     ($_SESSION['array'][1] == 'x' && $_SESSION['array'][2] == 'x' && $_SESSION['array'][3] == 'x') ||
     ($_SESSION['array'][4] == 'x' && $_SESSION['array'][5] == 'x' && $_SESSION['array'][6] == 'x') ||
-    ($_SESSION['array'][7] == 'x' && $_SESSION['array'][8] == 'x' && $_SESSION['array'][9] == 'x')
+    ($_SESSION['array'][7] == 'x' && $_SESSION['array'][8] == 'x' && $_SESSION['array'][9] == 'x') ||
+    ($_SESSION['array'][1] == 'x' && $_SESSION['array'][4] == 'x' && $_SESSION['array'][7] == 'x') ||
+    ($_SESSION['array'][2] == 'x' && $_SESSION['array'][5] == 'x' && $_SESSION['array'][8] == 'x') ||
+    ($_SESSION['array'][3] == 'x' && $_SESSION['array'][6] == 'x' && $_SESSION['array'][9] == 'x') ||
+    ($_SESSION['array'][1] == 'x' && $_SESSION['array'][5] == 'x' && $_SESSION['array'][9] == 'x') ||
+    ($_SESSION['array'][3] == 'x' && $_SESSION['array'][5] == 'x' && $_SESSION['array'][7] == 'x')
   ) {
     return 'x';
+  }
+
+  if (
+    ($_SESSION['array'][1] == 'o' && $_SESSION['array'][2] == 'o' && $_SESSION['array'][3] == 'o') ||
+    ($_SESSION['array'][4] == 'o' && $_SESSION['array'][5] == 'o' && $_SESSION['array'][6] == 'o') ||
+    ($_SESSION['array'][7] == 'o' && $_SESSION['array'][8] == 'o' && $_SESSION['array'][9] == 'o') ||
+    ($_SESSION['array'][1] == 'o' && $_SESSION['array'][4] == 'o' && $_SESSION['array'][7] == 'o') ||
+    ($_SESSION['array'][2] == 'o' && $_SESSION['array'][5] == 'o' && $_SESSION['array'][8] == 'o') ||
+    ($_SESSION['array'][3] == 'o' && $_SESSION['array'][6] == 'o' && $_SESSION['array'][9] == 'o') ||
+    ($_SESSION['array'][1] == 'o' && $_SESSION['array'][5] == 'o' && $_SESSION['array'][9] == 'o') ||
+    ($_SESSION['array'][3] == 'o' && $_SESSION['array'][5] == 'o' && $_SESSION['array'][7] == 'o')
+  ) {
+    return 'o';
   }
 }
